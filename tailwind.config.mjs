4caa0208d9d8a96dcb511eab8b62c87cb69a2013
Rian Anugrah <rian.te.quiero@gmail.com>
2024-09-15 19:26:34 +0700

    M works/grocery-go

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'playfair': ['Playfair Display', 'serif'],
+			},
+		},
 	},
 	plugins: [require('daisyui'),],
 	daisyui: {
