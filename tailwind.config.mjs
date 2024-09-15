8622d099138af06f2a347d30ef610ee4d1c544cc
Rian Anugrah <rian.te.quiero@gmail.com>
2024-09-15 12:46:59 +0700

    Fix Home Revision

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,3 +6,16 @@ export default {
 	},
 	plugins: [],
 }
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [daisyui],
+	daisyui: {
+		themes: ["light", "dark", "cupcake"], // Add any themes you want to use
+	},
+}
